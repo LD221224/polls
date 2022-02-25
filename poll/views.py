8237a417d875de21bf145a09471628975df27750
LD221224 <dayeong2815@gmail.com>
2022-02-25 12:47:25 +0900

    day38

--- poll/views.py
+++ poll/views.py
@@ -1,10 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from poll.models import Question
+
 
 def index(request):
-    return render(request, 'poll/index.html')
+    # 자료 전체 가져오기
+    question_list = Question.objects.all()
+    return render(
+        request,
+        'poll/index.html',
+        {'question_list': question_list}
+    )
     # return HttpResponse("<h1>안녕~ Django!!</h1>")
+
+
+def detail(request, pk):
+    # 자료 1개 가져오기
+    question = Question.objects.get(id=pk)
+    return render(
+        request,
+        'poll/detail.html',
+        {'question': question}
+    )
+
+
+def vote(request, pk):
+    pass
 
 
 def cart(request):
